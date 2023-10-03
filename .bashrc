6348b09a569b4d221c960d7a969bef5e120898df
EVassar <erlend.vassar@tptlive.ee>
2023-10-03 11:25:20 +0300

    Add Articles migration, seeder, factory

---
+++ .bashrc
@@ -0,0 +1,2 @@
+alias la='ls -a'
+alias pstorm="/C/users/erlen/Appdata/Local/Jetbrains/'PhpStorm 2023.1'/bin/phpstorm64.exe . &"
---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+@extends('partials.layout')
+@section('content')
+    <h1 class="text-3xl font-bold underline">
+        About
+    </h1>
+    <button class="btn">click me</button>
+@endsection
