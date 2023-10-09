e0af848c48392ac76c9ea2f18cdad9d49e1948a4
EVassar <erlend.vassar@tptlive.ee>
2023-10-09 15:21:36 +0300

    Remove not needed files

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Paginator::defaultView('partials.pagination');
+        Paginator::defaultSimpleView('partials.simple-pagination');
     }
 }
